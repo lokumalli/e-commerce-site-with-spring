9b9fae433ba49e49d92f474f2a1451ba0b6df2a7
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-06-16 07:00:45 +0530

    ide init

---
+++ src/test/java/com/example/ecommercesitewithspring/ECommerceSiteWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommercesitewithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceSiteWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
