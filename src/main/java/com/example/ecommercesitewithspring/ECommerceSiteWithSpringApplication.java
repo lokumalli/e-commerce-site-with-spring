9fe864b462a3a812a6f65a4f9cda5fd24bb595c1
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-06-16 07:01:05 +0530

    pom

--- src/main/java/com/example/ecommercesitewithspring/ECommerceSiteWithSpringApplication.java
+++ src/main/java/com/example/ecommercesitewithspring/ECommerceSiteWithSpringApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ECommerceSiteWithSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ECommerceSiteWithSpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceSiteWithSpringApplication.class, args);
+    }
 
 }
